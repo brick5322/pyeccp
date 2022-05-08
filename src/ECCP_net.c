d35c091fe1cfec65495131e36fbc451b572957e1
bric <bric5322@outlook.com>
2022-05-08 17:39:08 +0800

    补充了TODO注释块的内容

--- src/ECCP_net.c
+++ src/ECCP_net.c
@@ -4,11 +4,11 @@
 
 #include "ECCP_net.h"
 /**
- * @todo
- * @param socket
- * @param msg
- * @param IP_buffer
- * @return
+ * @todo 这里有个函数没写
+ * @param socket 提供的socket
+ * @param msg 调用者会提供了一块内存以接收msg
+ * @param IP_buffer 调用者提供一块内存接收点分十进制的IP
+ * @return msg的长度
  */
 int recv_eccp_msg(SOCKET socket,ECCP_message* msg,char* IP_buffer)
 {
@@ -18,11 +18,11 @@ int recv_eccp_msg(SOCKET socket,ECCP_message* msg,char* IP_buffer)
         return 0;
 }
 /**
- * @todo
- * @param socket
- * @param ip
- * @param msg
- * @return
+ * @todo 这里有个函数没写
+ * @param socket 提供的socket
+ * @param ip 要传达的IP地址，点分十进制“192.168.0.1:8080”
+ * @param msg 要传出的message
+ * @return 成功返回0，否则返回其他
  */
 int send_eccp_msg(SOCKET socket,const char *ip,ECCP_message* msg)
 {
